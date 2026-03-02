7ffd425c994d5c36f5ebbd5b1ed6442b20fb9611
Alfonso Moreno <alfonso.moreno@me.com>
2026-03-02 16:26:21 -0300

    Remove deprecated authentication endpoints: delete debug, sign-in, and sign-up APIs in favor of using Neon Auth client SDK directly from frontend

--- api/auth/sign-up.js
+++
@@ -1,7 +0,0 @@
-const { sendJson } = require('../_lib');
-
-module.exports = async (req, res) => {
-  sendJson(res, 410, {
-    error: 'Deprecated endpoint. Use Neon Auth client SDK directly from frontend.',
-  });
-};
